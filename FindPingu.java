a9e84035ff73e1f73f97039346aa1661376271e4
ppommer <pommer.paul@gmail.com>
2020-03-07 13:44:23 +0100

    first commit

---
+++ FindPingu.java
@@ -0,0 +1,91 @@
+
+@SuppressWarnings("serial")
+public class FindPingu extends Maze {
+  static int[][] maze;
+
+  public static void main(String[] args) {
+    int width = MiniJava.read("Breite des Labyrinths?");
+    int height = MiniJava.read("Höhe des Labyrinths?");;
+    maze = generateStandardPenguinMaze(width, height);
+    int maxDistance = MiniJava.read("Wie weit trauen Sie sich maximal weg vom Startpunkt?");
+    if(width < 3 || height < 3 || maxDistance < 1) {
+      MiniJava.write("Fehlerhafte Eingabe!");
+      return;
+    }
+    int penguins = walk(1, 0, maxDistance);
+    System.out.println("Gerettete Pinguine: " + penguins);
+  }
+
+  // test if there is a wall at the given coordinates
+  public static boolean wall(int x, int y) {
+    return maze[x][y] == WALL;
+  }
+
+  // test if there is no wall at the given coordinates; coordinates can be outside the maze
+  public static boolean noWall(int x, int y) {
+    if (x < 0 || y < 0 || x >= maze.length || y >= maze[0].length)
+      return false;
+    return maze[x][y] != WALL;
+  }
+
+  public static int walk(int x, int y, int maxDistance) {
+    if(maxDistance < 0)
+      return 0;
+
+    // are we going to walk out of the maze?
+    if (x < 0 || y < 0 || x >= maze.length || y >= maze[0].length)
+      return 0;
+
+    // are we going to walk on a field without wall?
+    if (noWall(x - 1, y - 1) && noWall(x - 1, y) && noWall(x - 1, y + 1) && noWall(x, y - 1)
+        && noWall(x, y) && noWall(x, y + 1) && noWall(x + 1, y - 1) && noWall(x + 1, y)
+        && noWall(x + 1, y + 1))
+      return 0;
+
+    // are we going to walk into a wall or on a field we have already visited?
+    if (maze[x][y] == WALL || maze[x][y] == OLD_PATH_ACTIVE || maze[x][y] == OLD_PATH_DONE) {
+      return 0;
+    }
+
+    int penguins = maze[x][y] == PENGUIN ? 1 : 0;
+
+    // we are walking on the field
+    maze[x][y] = PLAYER;
+    draw(maze);
+    maze[x][y] = OLD_PATH_ACTIVE;
+
+    penguins += walk(x + 1, y, maxDistance - 1);
+
+    // at this point, we have returned from recursion and re-visit the field
+    maze[x][y] = PLAYER;
+    draw(maze);
+    maze[x][y] = OLD_PATH_ACTIVE;
+
+    penguins += walk(x, y + 1, maxDistance - 1);
+
+    // at this point, we have returned from recursion and re-visit the field
+    maze[x][y] = PLAYER;
+    draw(maze);
+    maze[x][y] = OLD_PATH_ACTIVE;
+
+    penguins += walk(x - 1, y, maxDistance - 1);
+
+    // at this point, we have returned from recursion and re-visit the field
+    maze[x][y] = PLAYER;
+    draw(maze);
+    maze[x][y] = OLD_PATH_ACTIVE;
+
+    penguins += walk(x, y - 1, maxDistance - 1);
+
+    // at this point, we have returned from recursion and re-visit the field
+    maze[x][y] = PLAYER;
+    draw(maze);
+
+    // search in all directions completed
+    maze[x][y] = OLD_PATH_DONE;
+
+    return penguins;
+  }
+
+}
+
